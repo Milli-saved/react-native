97229f463fddfb48f89ee87f975ebea5d6c83c46
Milli-saved <million12.tenkir@gmail.com>
2024-10-01 14:56:36 +0300

    fix: add home button icon with style.

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,31 +1,43 @@
-import { View, Text, Image } from 'react-native'
-import { Tabs, Redirect } from 'expo-router'
-import {icons} from "../../constants"
+import { View, Text, Image } from "react-native";
+import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
 
-const TabIcon = ({icon, color, name, focused}) => {
-    return (
-        <View>
-            <Image
-                source={icon}
-            />
-        </View>
-    )
-}
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+          />
+          <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`} >
+              {name}
+          </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-      <Tabs>
-          <Tabs.Screen name='home' options={{
-              title: 'Home',
-              headerShown: false,
-              tabBarIcon: ({ color, focused }) => (
-                  <TabIcon icon={icons.home} color={color} name="Home"
-                  focused={focused}
-                  />
-              )
-          }} />
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          ),
+        }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
